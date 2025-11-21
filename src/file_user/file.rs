c04a024321aadead58cd71bb58784892a6c736d1
youpie <youplamb@hotmail.nl>
2025-11-21 19:53:20 +0100

    fixed creation date for new imports

--- src/file_user/file.rs
+++ src/file_user/file.rs
@@ -6,6 +6,12 @@ use sea_orm::ActiveValue::{NotSet, Set};
 
 use crate::{GenResult, encrypt_value};
 
+fn get_name(base_path: &PathBuf) -> Option<String> {
+    let mut path = base_path.clone();
+    path.push("kuma/name");
+    std::fs::read_to_string(path).ok()
+}
+
 pub fn load_user(
     path: &PathBuf,
 ) -> GenResult<(user_properties::ActiveModel, user_data::ActiveModel)> {
@@ -21,7 +27,7 @@ pub fn load_user(
         .var("CYCLE_TIME")
         .unwrap_or((env_map.var("KUMA_HEARTBEAT_INTERVAL")?.parse::<i32>()? - 500).to_string())
         .parse::<i32>()?
-        / 60;
+        / 60 / 60 * 60;
     let email_to = env_map.var("MAIL_TO")?;
     let new_shift = str_to_bool(
         env_map
@@ -48,18 +54,20 @@ pub fn load_user(
     let split_night_shift = str_to_bool(
         env_map
             .var("BREAK_UP_NIGHT_SHIFT")
-            .unwrap_or("true".to_owned()),
+            .unwrap_or("false".to_owned()),
     );
     let stop_night_shift = str_to_bool(
         env_map
             .var("STOP_SHIFT_AT_MIDNIGHT")
             .unwrap_or("false".to_owned()),
     );
+    let name = get_name(&path);
     let mut execution_min_path = path.clone();
     execution_min_path.push("kuma");
     execution_min_path.push("starting_minute");
     let execution_min = std::fs::read_to_string(execution_min_path)?.parse::<i32>()?;
     let user_properties = user_properties::ActiveModel {
+        user_properties_id: NotSet,
         execution_interval_minutes: Set(cycle_time),
         execution_minute: Set(execution_min),
         send_error_mail: Set(error_mail),
@@ -70,9 +78,10 @@ pub fn load_user(
         send_welcome_mail: Set(welcome_mail),
         stop_midnight_shift: Set(stop_night_shift),
         split_night_shift: Set(split_night_shift),
-        ..Default::default()
+        auto_delete_account: Set(true)
     };
     let user_data = user_data::ActiveModel {
+        user_data_id: NotSet,
         user_name: Set(username
             .parse::<u32>()
             .and_then(|name| Ok(name.to_string()))
@@ -81,8 +90,12 @@ pub fn load_user(
         password: Set(encrypt_value(&password)?),
         email: Set(encrypt_value(&email_to)?),
         file_name: Set(filename.unwrap_or(username)),
+        name: Set(name.map(|name| encrypt_value(&name).unwrap_or_default())),
+        custom_general_properties: NotSet,
         user_properties: NotSet,
-        ..Default::default()
+        last_execution_date: NotSet,
+        last_succesfull_sign_in_date: NotSet,
+        creation_date: Set(chrono::offset::Utc::now().naive_utc())
     };
     Ok((user_properties, user_data))
 }
